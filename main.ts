9a5ba1d0cb120014364dc46559e6363a8890b724
almas-aftab-ahmed <almassiddiqui375@mail.com>
2024-11-13 19:10:03 -0800

    commit message

--- main.ts
+++ main.ts
@@ -15,20 +15,20 @@ form.addEventListener('submit', (event: Event) => {
 
     // Generating the resume content dynamically
     const resumehtml = `
-        <h2><b>RESUME</b></h2>
+        <h2><b> Editeable RESUME</b></h2>
         <h3>Personal Information</h3>
-        <p><b>Name:</b> ${name}</p>
-        <p><b>Email:</b> ${email}</p>
-        <p><b>Phone:</b> ${phone}</p>
+        <p><b>Name:</b><span contenteditable="true"> ${name}</span</p>
+        <p><b>Email:</b><span contenteditable="true">  ${email}</span></p>
+        <p><b>Phone:</b><span contenteditable="true">  ${phone}</span></p>
 
         <h3>Qualifications</h3>
-        <p>${education}</p>
+        <p  contenteditable="true">${education}</p>
 
         <h3>Experience</h3>
-        <p>${experience}</p>
+        <p  contenteditable="true">${experience}</p>
 
         <h3>Skills</h3>
-        <p>${skills}</p>
+        <p  contenteditable="true">${skills}</p>
     `;
 
     // Displaying generated resume
